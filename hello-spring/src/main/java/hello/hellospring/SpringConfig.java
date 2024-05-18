8544d4b336267d428db4a37ea0982d092af4a604
Joo-hyun-Kang <autoba9687@gmail.com>
2024-05-19 02:48:02 +0900

    feat: change db-connecter by jdbc template for simplification

--- hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -3,6 +3,7 @@ package hello.hellospring;
 import hello.hellospring.domain.MemberRepository;
 import hello.hellospring.domain.MemoryMemberRepository;
 import hello.hellospring.repository.JdbcMemberRepository;
+import hello.hellospring.repository.JdbcTemplateMemberRepository;
 import hello.hellospring.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -25,6 +26,7 @@ public class SpringConfig {
     @Bean
     public MemberRepository memberRepository() {
         //return new MemoryMemberRepository();
-        return new JdbcMemberRepository(dataSource);
+        //return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 }
