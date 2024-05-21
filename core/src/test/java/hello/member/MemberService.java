7c1662710cbca0c7acbe8b7acb0037bee522aebc
Joo-hyun-Kang <autoba9687@gmail.com>
2024-05-21 20:49:08 +0900

    feat: implement class related with member

---
+++ core/src/test/java/hello/member/MemberService.java
@@ -0,0 +1,16 @@
+package hello.member;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberService {
+
+    @Test
+    void join() {
+    }
+
+    @Test
+    void findMember() {
+    }
+}
