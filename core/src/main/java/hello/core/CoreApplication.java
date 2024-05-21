6c0b12bef97dccea1b724b1f28d769683c6a2d36
Joo-hyun-Kang <autoba9687@gmail.com>
2024-05-21 22:10:20 +0900

    test: test memberService, memberRepository

--- core/src/main/java/hello/core/CoreApplication.java
+++ core/src/main/java/hello/core/CoreApplication.java
@@ -1,5 +1,9 @@
 package hello.core;
 
+import hello.member.Grade;
+import hello.member.Member;
+import hello.member.MemberService;
+import hello.member.MemberServiceImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +11,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CoreApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CoreApplication.class, args);
+		MemberService memberService = new MemberServiceImpl();
+		Member member = new Member(1L, "memberA", Grade.VIP);
+		memberService.join(member);
+
+		Member findMember = memberService.findMember(1L);
+		System.out.println("new Member = " + member.getName());
+		System.out.println("find Member = " + findMember.getName());
 	}
 
 }
--- core/src/test/java/hello/member/MemberService.java
+++ core/src/test/java/hello/member/MemberService.java
@@ -1,16 +1,23 @@
 package hello.member;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class MemberService {
+class MemberServiceTest {
+    MemberService memberService = new MemberServiceImpl();
 
     @Test
     void join() {
-    }
+        //given
+        Member member = new Member(1L, "memberA", Grade.VIP);
 
-    @Test
-    void findMember() {
+        //when
+        memberService.join(member);
+        Member findMember = memberService.findMember(1L);
+
+        //then
+        Assertions.assertThat(findMember).isEqualTo(member);
     }
 }
