972cd83d0d18d67acdb956351f1fcf261b205582
Joo-hyun-Kang <autoba9687@gmail.com>
2024-05-22 22:15:05 +0900

    fix: refectoring appConfig

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -2,20 +2,31 @@ package hello.core;
 
 import hello.Order.OrderService;
 import hello.Order.OrderServiceImpl;
+import hello.discount.DiscountPolicy;
+import hello.discount.FixDiscountPolicy;
 import hello.discount.RateDiscountPolicy;
+import hello.member.MemberRepository;
 import hello.member.MemberService;
 import hello.member.MemberServiceImpl;
 import hello.member.MemoryMemberRepository;
 
 public class AppConfig {
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
     public OrderService orderService() {
         return new OrderServiceImpl(
-                new MemoryMemberRepository(),
-                new RateDiscountPolicy()
+                memberRepository(),
+                discountPolicy()
         );
+    }
+
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
